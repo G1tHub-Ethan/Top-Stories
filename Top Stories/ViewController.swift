8c8910e6c72da559b790c280ffc92f039275bd77
Ethan Yu <jethanyu@outlook.com>
2020-07-30 10:56:28 -0500

    Initial Commit

---
+++ Top Stories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Top Stories
+//
+//  Created by Ethan Yu on 7/30/20.
+//  Copyright © 2020 Ethan Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
